1e212a531938ced8fae37acb95490a44905651a5
rkhemka <rajatkhemka66@gmail,com>
2017-08-06 23:22:47 -0700

    Push Notification Using Firebase Added

---
+++ app/src/main/java/com/androidevelopers/cs5540/businessexchange/Adapters/NotificationAdapter.java
@@ -0,0 +1,27 @@
+package com.androidevelopers.cs5540.businessexchange.Adapters;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by rajat on 8/6/2017.
+ */
+
+public class NotificationAdapter extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
+
+
+
